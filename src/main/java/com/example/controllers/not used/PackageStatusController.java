7bebe1f949f1f40db0628db1a1f0aa9a0d2c1610
Artyom <artemmindov@ya.ru>
2024-06-29 03:23:18 +0300

    работает

--- src/main/java/com/example/controllers/not used/PackageStatusController.java
+++
@@ -1,9 +0,0 @@
-
-// src/main/java/com/example/controllers/PackageStatusController.java
-package com.example.controllers;
-
-import com.example.models.PackageStatus;
-
-public class PackageStatusController {
-    // Реализация методов CRUD для статусов посылок
-}
